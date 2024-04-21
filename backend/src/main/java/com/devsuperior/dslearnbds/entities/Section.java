a654a6d8e0a7d0231dd42eba614d627c65ede54a
IgorTecnologia <iguigu2019@gmail.com>
2024-04-21 15:10:09 -0300

    Section Entity

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -0,0 +1,125 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_section")
+public class Section  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String title;
+	private String description;
+	private Integer position;
+	private String imgUri;
+	
+	@ManyToOne
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
+	
+	@ManyToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
+	
+	public Section() {
+	}
+
+	public Section(Long id, String title, String description, Integer position, String imgUri, Resource resource,
+			Section prerequisite) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.position = position;
+		this.imgUri = imgUri;
+		this.resource = resource;
+		this.prerequisite = prerequisite;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	public String getImgUri() {
+		return imgUri;
+	}
+
+	public void setImgUri(String imgUri) {
+		this.imgUri = imgUri;
+	}
+
+	public Resource getResource() {
+		return resource;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+
+	public Section getPrerequisite() {
+		return prerequisite;
+	}
+
+	public void setPrerequisite(Section prerequisite) {
+		this.prerequisite = prerequisite;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Section other = (Section) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
