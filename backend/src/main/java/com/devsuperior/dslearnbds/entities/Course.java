24604df10141fa04b896cafe8a2aba6ad82df401
IgorTecnologia <iguigu2019@gmail.com>
2024-04-21 13:15:43 -0300

    Course Entity

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -0,0 +1,85 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_course")
+public class Course implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String imgUrl;
+	private String imgGrayUri;
+	
+	public Course() {
+	}
+
+	public Course(Long id, String name, String imgUrl, String imgGrayUri) {
+		this.id = id;
+		this.name = name;
+		this.imgUrl = imgUrl;
+		this.imgGrayUri = imgGrayUri;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getImgGrayUri() {
+		return imgGrayUri;
+	}
+
+	public void setImgGrayUri(String imgGrayUri) {
+		this.imgGrayUri = imgGrayUri;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
