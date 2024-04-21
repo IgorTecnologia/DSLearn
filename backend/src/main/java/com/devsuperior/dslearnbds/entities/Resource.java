7cd44f89e86c5ed7d4102408f2c83fb08669c60e
IgorTecnologia <iguigu2019@gmail.com>
2024-04-21 15:09:42 -0300

    Resource Entity

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearnbds.enums.ResourceType;
@@ -31,6 +34,9 @@ public class Resource implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "offer_id")
 	private Offer offer;
+	
+	@OneToMany(mappedBy = "resource")
+	private List<Section> sections = new ArrayList<>();
 	
 	public Resource() {
 	}
