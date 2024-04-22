eb60696b37ef8e435528cccdb69ba16a4ab71901
IgorTecnologia <iguigu2019@gmail.com>
2024-04-21 22:38:27 -0300

    Enrollment Entity

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
@@ -24,6 +27,9 @@ public class Enrollment {
 	
 	private boolean avaliable;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment() {
 	}
