fdd291179a091ccb263f569dcebd0f7e6586e1e4
IgorTecnologia <iguigu2019@gmail.com>
2024-04-23 10:51:58 -0300

    Notification Repository

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Notification;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long>{
+
+}
