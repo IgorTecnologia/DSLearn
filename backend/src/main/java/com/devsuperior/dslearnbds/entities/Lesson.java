5b1e4eacdedaf0d99575cbd958fda6b332f45c23
IgorTecnologia <iguigu2019@gmail.com>
2024-04-23 10:47:22 -0300

    Lesson Entity

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -15,6 +17,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -43,6 +46,12 @@ public abstract class Lesson  implements Serializable {
 		}
 	)
 	private Set<Enrollment> enrollmentsDone = new HashSet<>();
+	
+	@OneToMany(mappedBy = "lesson")
+	private List<Topic> topics = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "lesson")
+	private List<Deliver> deliveries = new ArrayList<>();
 	
 	public Lesson() {
 	}
@@ -88,6 +97,14 @@ public abstract class Lesson  implements Serializable {
 
 	public Set<Enrollment> getEnrollmentsDone() {
 		return enrollmentsDone;
+	}
+	
+	public List<Topic> getTopics() {
+		return topics;
+	}
+
+	public List<Deliver> getDeliveries() {
+		return deliveries;
 	}
 
 	@Override
