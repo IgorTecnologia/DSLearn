6c620728ccfade811cb9f8950ff0bb65e9a3d1bd
IgorTecnologia <iguigu2019@gmail.com>
2024-04-23 14:06:42 -0300

    User Entity

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -149,4 +149,13 @@ public class User implements UserDetails, Serializable {
 		
 		return true;
 	}
+	
+	public boolean hasRole(String roleName) {
+		for(Role role: roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
