a01a98737ba9f6b0f17416c50b8e3c1f83b8bcd7
IgorTecnologia <iguigu2019@gmail.com>
2024-04-23 16:12:21 -0300

    Deliver Resource

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dslearnbds.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
+import com.devsuperior.dslearnbds.services.DeliverService;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+
+	@Autowired
+	private DeliverService service;
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> saveRevision (@PathVariable Long id, @RequestBody DeliverRevisionDTO dto){
+		service.saveRevision(id, dto);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+}
