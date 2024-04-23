d219fe46cb546fc7a458a63a6c6f6f7f98e0d8e8
IgorTecnologia <iguigu2019@gmail.com>
2024-04-23 14:09:38 -0300

    User Service

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -20,8 +20,13 @@ public class UserService implements UserDetailsService {
 
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 
+	@Autowired
+	private AuthService authService;
+	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		
+		authService.validateSelfOrAdmin(id);
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
@@ -41,5 +46,6 @@ public class UserService implements UserDetailsService {
 		logger.info("User found: " + username);
 		return user;
 	}
-
+	
+	
 }
