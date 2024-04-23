c2e5237fb03b52286c0351a3e0196ccda0f0c25f
IgorTecnologia <iguigu2019@gmail.com>
2024-04-23 15:30:59 -0300

    Notification Service

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private NotificationRepository repository;
+	
+	@Autowired
+	private AuthService authSevice;
+	
+	@Transactional(readOnly = true)
+	public Page<NotificationDTO> notificationForCurrentUser(Pageable pageable){
+		
+		User user = authSevice.authenticated();
+		
+		Page<Notification> page = repository.findByUser(user, pageable);
+		
+		return page.map(x -> new NotificationDTO(x));
+	}
+		
+	
+}
