8391a8bf6d6d913fc9ac41cb8e250968f766cf90
IgorTecnologia <iguigu2019@gmail.com>
2024-05-02 19:41:43 -0300

    Notification Repository

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
@@ -3,12 +3,17 @@ package com.devsuperior.dslearnbds.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslearnbds.entities.Notification;
 import com.devsuperior.dslearnbds.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
 
-	Page<Notification> findByUser(User user, Pageable pageable);
+	@Query("SELECT obj FROM Notification obj WHERE "
+	        + "(obj.user = :user) AND "
+	        + "(:unreadOnly = false OR obj.read = false) "
+	        + "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 	
 }
